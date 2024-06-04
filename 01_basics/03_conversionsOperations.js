49fc20377e3fcaae26e28c70a16238504897d40d
mms <106025061+mehtab318@users.noreply.github.com>
2024-06-04 07:16:40 +0000

    dataTypes

--- 01_basics/03_conversionsOperations.js
+++ 01_basics/03_conversionsOperations.js
@@ -1,4 +1,4 @@
-let score = "hitesh";
+let score = "33abc";
 // console.log(typeof score);
 // console.log(typeof (score));
 
@@ -13,22 +13,25 @@ let score = "hitesh";
 // "33abc" =>NaN
 //true =>   1;false => 0
 
+// ------> this function is for converting string into Boolean id we convert 1(number) into Boolean it will be show [true] and,
+// similarly while we convert 0{zero number} into Boolean it will show [false] and if we convert empty string means no value in variale it will show [false]
+let IsLoggedIn = 33;
+let BooleanIsLoggedIn = Boolean(IsLoggedIn)
+console.log(BooleanIsLoggedIn)
+console.log(typeof BooleanIsLoggedIn)
 
-// let isLoggedIn = "mehtabsiddiqui"
-
-// let booleanIsLoggedIn = Boolean(isLoggedIn)
-// console.log(booleanIsLoggedIn);
-
-
-// 1 => true 
-// 0 => false 
-// ""(empty string) => false 
+// =>notes for the above function
+// 1 => true
+// 0 => false
+// ""(empty string) => false
 // "string" => true
+// but if the value is like (33abc not in double quotes) it will show error {means unexpected taken}
 
 
 
-let someNumber =33
 
-let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// ---> this function is for converting numbrer into string
+// let someNumber = 33;
+// let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
