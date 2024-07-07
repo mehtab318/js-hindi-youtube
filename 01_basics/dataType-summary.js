e7b2490eaf91ff4bc1592329fb9a2c2e6cd73dbf
mms <106025061+mehtab318@users.noreply.github.com>
2024-07-07 09:24:33 +0000

    summary

---
+++ 01_basics/dataType-summary.js
@@ -0,0 +1,42 @@
+
+// JavaScript is a dynamically typed language, which means that data types of
+//  variables are determined by the value they hold at runtime and can change throughout the program as we assign different values to them.
+
+
+
+
+// there are two type of data type in javascript 
+// 1. primitive 
+// 2.non-primitive 
+
+
+// *****************primitive*****************
+
+// #primitive (reference) type datatype :- jitne bhi primitive data type hote hai woh call by reference hote hai.jo original
+//  data hota hai memory ka woh hume nahi diya jaata, uska sirf reference diya jaata hai (original ka reference diya jaata).
+// there are 7 types of primitive datatype
+// 1) string => returns string value 
+// 2)number => returns number.
+// 3) boolean => returns alse and true value 
+// 4) null => returns (object) note:- equal to operator (==) and datype operator(===) treats different null value.
+// 5)undefined => returns undefined values
+// 6)symbol => it returns symbol (it returns symbol DataType) note:- this DataType means for unique function.
+// 7)bigint => it returns Bigint(this is for bignumber that is so so big that doesnt store in nmber datatype).
+
+
+// example of primitive datatype=>
+
+// string 
+const  = ;
+console.log(typeof bigNumber )
+
+
+// *****************non-primitive*****************
+// #non-primitive 
+
+
+// 1.array
+// 2.object
+// 3.function
+
+
