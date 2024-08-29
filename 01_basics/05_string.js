0827f4d872de90a743ad9117fa324060c3708dd1
mms <106025061+mehtab318@users.noreply.github.com>
2024-08-29 04:10:20 +0000

    string upates

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -1,11 +1,11 @@
-const name = "mehtab siddiqui"
-const repocount = 50
+// const name = "mehtab siddiqui"
+// const repocount = 50
 // console.log(name + repocount) note:- this is the bad way to declatre and concatinat string
 
 // console.log(`hello my name is ${name} nd my repocount is ${repocount}`) 
 // by string interpolition method we can declate string easily and this string is readable for others also
 
-const gamename = new String('mehtab-hc')
+// const gamename = new String('mehtab-hc')
 // console.log(gamename[0])
 // console.log(gamename.__proto__)
 // console.log(gamename.length)
@@ -22,18 +22,26 @@ const gamename = new String('mehtab-hc')
 
 
 
-const newstring = "     hitesh    "
+// const newstring = "     hitesh    "
 // console.log(newstring)
 // console.log(newstring.trimStart())
 // console.log(newstring.trimEnd())
 
 
 
-const url = "https://hitesh.com/hitesh%20choudhary"
-console.log(url.replace('%hitesh', 'mehtab'))
+// const url = "https://hitesh.com/hitesh%20choudhary"
+// console.log(url.replace('%hitesh', 'mehtab'))
 
 
-let a = {
-    name: "mehtab",
-    language: 'javascript',
-}
+// let a = {
+//     name: "mehtab",
+//     language: 'javascript',
+// }
+
+
+// *********************************Date:-29/08/24*************************************************
+
+// STRING
+const name = "mehtab"
+const repocount = 40
+console.log(`hello my name is ${name} and my repocount is ${repocount}`)//this is string interpolation method to concatinate strings
